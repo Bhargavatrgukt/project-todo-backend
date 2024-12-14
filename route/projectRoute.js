ad7b5ceff87d3b72857c95ac10806dd150a6cc15
Bhargav <bhargavatrgukt@gmail.com>
2024-12-14 12:33:59 +0530

    added middleware for routes

--- route/projectRoute.js
+++ route/projectRoute.js
@@ -5,14 +5,15 @@ import {
   getProjects,
   updateProject,
 } from "../controller/projectController.js";
+import { validateRequest } from "../middleware/requestValidations.js";
 
 const router = express.Router();
 
-router.post("/projects", createProject);
+router.post("/projects", validateRequest, createProject);
 
 router.get("/projects", getProjects);
 
-router.put("/projects/:id", updateProject);
+router.put("/projects/:id", validateRequest, updateProject);
 
 router.delete("/projects/:id", deleteProject);
 
